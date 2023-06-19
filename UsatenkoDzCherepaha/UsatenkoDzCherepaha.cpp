0e0b8920e2449c0f8e16608638928c27b1f4ec17
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-19 19:48:39 +0300

    Добавьте файлы проекта.

---
+++ UsatenkoDzCherepaha/UsatenkoDzCherepaha.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double day , vremya;
+    cout << "Введiть кiлькiсть днiв , скiльки черепаха шла: "<<endl;
+    cin >> vremya;
+    for (double i = 1.5;i < vremya;i += 0.2) {
+        cout <<"Cтiльки черепаха проходила за наступнi днi:  " << i << endl;
+    }
+
+}
+
+
